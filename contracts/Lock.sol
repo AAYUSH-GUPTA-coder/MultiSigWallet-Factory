f08627004dce0a431754f601b2082a3fb67ed20a
AAYUSH-GUPTA-coder <60979345+AAYUSH-GUPTA-coder@users.noreply.github.com>
2022-11-29 00:35:10 +0530

    Update Lock.sol

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -5,16 +5,16 @@ pragma solidity ^0.8.9;
 contract MultiSigWallet {
     // recive crypto in wallet
     event Deposit(address indexed sender, uint amount);
-    // submit when one user submit a transcation and waiting for other stake holders to vote, yes or no
+    // submit when one user submit a transaction and waiting for other stake holders to vote, yes or no
     event Submit(uint indexed txId);
-    // Other owners when approve the transcation
+    // Other owners when approve the transaction
     event Approve(address indexed owner, uint indexed txId);
-    // once the transcation is approved, they can also revoke the transcation
-    event Revoke(address indeved owner, uint indexed txId);
+    // once the transaction is approved, they can also revoke the transaction
+    event Revoke(address indexed owner, uint indexed txId);
     // once the sufficient amount of approval, then the contract can be executed
     event Execute(uint indexed txId);
 
-    struct Transcation {
+    struct transaction {
         address to;
         uint value;
         bytes data;
@@ -25,22 +25,37 @@ contract MultiSigWallet {
     address[] public owners;
     // mapping to check is this address, a onwer in multiSig wallet
     mapping (address => bool) public isOwner;
-    // number of votes required to transcation to pass
+    // number of votes required to transaction to pass
     uint public required;
 
-    // array of transcation
-    Transcation[] public transcations;
+    // array of transaction
+    transaction[] public transactions;
     // mapping(txId => mapping(address_of_owner => yes/no)) public approve;
-    mapping(uint => mapping(address => bool)) public approve
+    mapping(uint => mapping(address => bool)) public approved;
 
     modifier onlyOwner() {
         require(isOwner[msg.sender], "not owner");
         _;
     }
 
+    modifier txExists(uint _txId){
+        require(_txId < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier notApproved(uint _txId){
+        require(!approved[_txId][msg.sender], "tx already approved");
+        _;
+    }
+
+    modifier notExecuted(uint _txId) {
+        require(!transactions[_txId].executed, "tx already executed");
+        _;
+    }
+
     /**
      * @dev contructor
-     * @param array of owner addresses, no of votes required
+     * @param _owners: array of owner addresses,_required: no of votes required
      */
     constructor(address[] memory _owners, uint _required){
         require(_owners.length > 0, "onwers required");
@@ -48,37 +63,78 @@ contract MultiSigWallet {
 
         for(uint i; i< _owners.length; i++){
             address owner = _owners[i];
-            require(onwer != address(0), "invalid owner!");
+            require(owner != address(0), "invalid owner!");
             require(!isOwner[owner], "owner is not unique");
 
-            isOwner[onwer] = true;
-            owners.push(onwer);
+            isOwner[owner] = true;
+            owners.push(owner);
         }
 
         required = _required;
     }
 
     /**
-     * @dev submit the transcation
+     * @dev submit the transaction
      * @param _to: address of reciver
      * _value: amount to send
      * _data: any data you want to send
      */
     function submit(address _to, uint _value, bytes calldata _data) external onlyOwner{
-        transcations.push(Transcation({
+        transactions.push(transaction({
             to:_to,
             value:_value,
-            data:_data
+            data:_data,
             executed:false
         }));
-        emit Submit(transcation.length - 1);
+        emit Submit(transaction.length - 1);
     }
 
-    function approve(uint _txId)
+    /**
+     * @dev function to allow onwers to approve transcation 
+     */
+    function approve(uint _txId) external onlyOwner txExists(_txId) notApproved(_txId) notExecuted(_txId){
+        approved[_txId][msg.sender] = true;
+        emit Approve(msg.sender , _txId);
+    }
+
+    /**
+     * @dev function to get count how many voted for approval
+     * @param _txId : transcation ID
+     */
+
+    function _getApprovalCount(uint _txId) private view returns(uint count){
+        for(uint i; i< owners.length; i++){
+            if(approved[_txId][owners[i]]){
+                count += 1;
+            }
+        }
+    }
+
+    /**
+     * @dev function to execute the transcation
+     * @param _txId : transcation ID
+     */
+    function execute(uint _txId) external txExists(_txId) notExecuted(_txId) {
+        Transaction storage transaction = transactions[_txId];
+        require(_getApprovalCount(_txId) >= required, "not enough approvals");
+        // Transaction storage transaction = transactions[_txId];
+        transaction.executed = true;
+        (bool success, ) = transaction.to.call{value: transaction.value}(transaction.data);
+        require(success, "tx failed");
+        emit Execute(_txId);
+    }
+
+    /**
+     * @dev function to execute revoke. Allow user to revoke approval if she earlier approve the transaction
+     * @param _txId : transcation ID
+     */
+    function revoke(uint _txId) external onlyOwner txExists(_txId) notExecuted(_txId){
+        require(approved[_txId][msg.sender], "tx not approved");
+        approved[_txId][msg.sender] = false;
+        emit Revoke(msg.sender, _txId);
+    }
 
     receive() external payable {
-        emit Deposit(msg.sender, msg.value)
+        emit Deposit(msg.sender, msg.value);
     }
-
-
 }
